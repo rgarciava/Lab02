d9df2f1142faf5bfc3694d4cc41ff662b4821945
Ronald Pablo Garcia Valdivia <rgarciava@unsa.edu.pe>
2024-05-21 10:17:27 -0500

    Se creó el método para Invertir un numero de forma recursiva (no usar String)

---
+++ Invertir.java
@@ -0,0 +1,28 @@
+public class Invertir {
+
+    public static void main(String[] args) {
+        System.out.print(invertir(234324, index(234324)));
+        // System.out.print(234324%100);
+
+    }
+
+    public static int invertir(int n, int c) {
+        if (n < 10) {
+            return n;
+        }
+        return (n % 10) * (int) (Math.pow(10, c - 1)) + invertir(n / 10, c - 1);
+    }
+
+    public static int index(int n) {
+
+        if (n == 0) {
+            return 1;
+        }
+        int contador = 0;
+        while (n != 0) {
+            n = n / 10;
+            contador++;
+        }
+        return contador;
+    }
+}
